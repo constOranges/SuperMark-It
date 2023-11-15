85327b9da53d8bce0995380b1f0b4a105ce211c4
Ryan McMurtry <rkainoam@gmail.com>
2023-11-14 19:34:01 -0800

    created base components for homepage

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "../header/Header.scss";
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+
+export default Header;
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../navbar/Navbar.scss";
+
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+
+export default Navbar;
