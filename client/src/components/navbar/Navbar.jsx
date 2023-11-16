77a7078c67c300abe583b1cf4e155d03bcb03182
Sarah Shu <sarahjadeshu@gmail.com>
2023-11-15 22:12:17 -0800

    added new item component and some routes, linked to icons in navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -10,37 +10,45 @@ import "./Navbar.scss";
 
 
 const Navbar = () => {
-    return(
-        <div className="navbar">
-            <div className="wrapper">
-                <div className="left">
-                    <Link className="link" to="/">SUPER MARK-IT</Link>
-                </div>
-                <div className="right">
-                    <div className="icons">
-                        <div className="homeIcon">
-                            <HomeRoundedIcon />
-                        </div>
-                        <div className="addIcon">
-                            <AddBoxRoundedIcon />
-                        </div>
-                        <div className="searchIcon">
-                            <SearchRoundedIcon />
-                        </div>
-                        <div className="favIcon">
-                            <FavoriteRoundedIcon />
-                        </div>
-                        <div className="notifIcon">
-                            <NotificationsRoundedIcon />
-                        </div>
-                        <div className="profileIcon">
-                            <PersonRoundedIcon />
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div className="navbar">
+        <div className="wrapper">
+          <div className="left">
+            <Link className="link" to="/">
+              SUPER MARK-IT
+            </Link>
+          </div>
+          <div className="right">
+            <div className="icons">
+              <div className="homeIcon">
+                <Link className="link" to="/">
+                  <HomeRoundedIcon />
+                </Link>
+              </div>
+              <div className="addIcon">
+                <Link className="link" to="/add">
+                  <AddBoxRoundedIcon />
+                </Link>
+              </div>
+              <div className="searchIcon">
+                <Link className="link" to="/search">
+                  <SearchRoundedIcon />
+                </Link>
+              </div>
+              <div className="favIcon">
+                <FavoriteRoundedIcon />
+              </div>
+              <div className="notifIcon">
+                <NotificationsRoundedIcon />
+              </div>
+              <div className="profileIcon">
+                <PersonRoundedIcon />
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 
---
+++ client/src/components/search/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Search.scss";
+
+const Search = () => {
+    return (
+        <div className="search">Search</div>
+    )
+}
+
+export default Search;
