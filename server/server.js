8d49d16df64ce30317961b863032a6a4f045d254
shu-william <williamshu@outlook.com>
2023-11-16 11:24:56 -0800

    added swagger documentation structure to backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const port = 8000;
 app.use(cookieParser());
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.CLIENT_URI, credentials: true }));
 
 require('./config/mongoose.config');
-require("./routes/user.route")(app);
+require('./routes/user.route')(app);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
