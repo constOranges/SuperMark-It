11c2e1f21cb7053553b552b8c6d1c74e2b2fa37a
shu-william <williamshu@outlook.com>
2023-12-05 00:21:00 -0800

    adjusted deletion routes so categories, lists, and items get updated properly upon deletion of anything

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -20,20 +20,52 @@ module.exports.addCategory = (req, res) => {
         })
 }
 
-// Update this so that it removes category ID from inUseIDs in each occupying item
-module.exports.removeCategory = (req, res) => {
-    User.findByIdAndUpdate(req.userId,
-        {
-            $pull:
+module.exports.removeCategory = async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        User.findByIdAndUpdate(req.userId,
             {
-                categories:
-                    { '_id': req.body.categoryId }
-            }
-        }).then(user => {
-            res.status(200).json({ message: "Category removed succesfully." });
-        }).catch(err => {
-            res.status(400).json(err);
-        })
+                $pull:
+                {
+                    categories:
+                        { '_id': req.body.categoryId }
+                }
+            }).then(user => {
+                console.log("Category deleted successfully from user document.");
+            }).catch(err => console.log(err))
+
+        Item.find({ inUseIDs: { $in: [req.body.categoryId] }})
+            .then(async items => {
+                for (const item of items) {
+                    let returnedItem = await Item.findByIdAndUpdate(
+                        item._id,
+                        { $pull: { inUseIDs: req.body.categoryId } },
+                        { new: true }
+                    );
+
+                    // deletes item permanently if no longer in use by any categories or lists
+                    if (returnedItem.inUseIDs.length === 0) {
+                        try {
+                            await Item.findByIdAndDelete(item._id);
+                            console.log("Item deleted permanently.");
+                        } catch (err) {
+                            console.error("Error deleting item permanently:", err.message);
+                        }
+                    }
+                }
+                console.log("Items updated successfully.");
+            }).catch(err => console.log(err))
+
+        await session.commitTransaction();
+        session.endSession();
+        res.status(200).json({ message: "Category removed succesfully." });
+    } catch (err) {
+        await session.abortTransaction();
+        session.endSession();
+        res.status(400).json(err);
+    }
 }
 
 // Ask whether to have separate routes for name and icon or just one update route
--- server/controllers/list.controller.js
+++ server/controllers/list.controller.js
@@ -20,17 +20,50 @@ module.exports.addList = (req, res) => {
         })
 }
 
-module.exports.removeList = (req, res) => {
-    User.findByIdAndUpdate(req.userId,
-        {
-            $pull:
+module.exports.removeList = async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        User.findByIdAndUpdate(req.userId,
             {
-                lists:
-                    { '_id': req.body.listId }
-            }
-        }).then(user => {
-            res.status(200).json({ message: "List removed succesfully." });
-        }).catch(err => {
-            res.status(400).json(err);
-        })
+                $pull:
+                {
+                    lists:
+                        { '_id': req.body.listId }
+                }
+            }).then(user => {
+                console.log("List deleted successfully from user document.");
+            }).catch(err => console.log(err))
+
+        Item.find({ inUseIDs: { $in: [req.body.listId] }})
+            .then(async items => {
+                for (const item of items) {
+                    let returnedItem = await Item.findByIdAndUpdate(
+                        item._id,
+                        { $pull: { inUseIDs: req.body.listId } },
+                        { new: true }
+                    );
+
+                    // deletes item permanently if no longer in use by any categories or lists
+                    if (returnedItem.inUseIDs.length === 0) {
+                        try {
+                            await Item.findByIdAndDelete(item._id);
+                            console.log("Item deleted permanently.");
+                        } catch (err) {
+                            console.error("Error deleting item permanently:", err.message);
+                        }
+                    }
+                }
+                console.log("Items updated successfully.");
+            }).catch(err => console.log(err))
+
+        await session.commitTransaction();
+        session.endSession();
+        res.status(200).json({ message: "List removed succesfully." });
+    } catch (err) {
+        await session.abortTransaction();
+        session.endSession();
+        res.status(400).json(err);
+    }
 }
