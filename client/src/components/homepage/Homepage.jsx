05aed7949975172b5329fb14c8d14b40f9baf374
Ryan McMurtry <rkainoam@gmail.com>
2024-01-11 19:07:48 -0800

    installed jquery and stylized toggle visibility

--- client/src/components/homepage/Homepage.jsx
+++ client/src/components/homepage/Homepage.jsx
@@ -4,42 +4,48 @@ import "../homepage/Homepage.scss";
 import axios from "axios";
 import Options from "../options/Options";
 import ListOptions from "../listOptions/ListOptions";
+import $ from "jquery";
 
 const Homepage = () => {
+  const [user, setUser] = useState(null);
+  const [kitchen, setKitchen] = useState(true);
 
-    const [user, setUser] = useState(null);
-    const [kitchen, setKitchen] = useState(true);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_REACT_APP_API_URL}/api/users/currentuser`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUser(res.data.user);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios
-        .get(`${import.meta.env.VITE_REACT_APP_API_URL}/api/users/currentuser`, {
-            withCredentials: true,
-        })
-        .then((res) => {
-            setUser(res.data.user)
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }, []);
-
-    
+  $(".link").on("click", function () {
+    $(".link").removeClass("selected");
+    $(this).addClass("selected");
+  });
 
   return (
     <div className="homepage">
       <div className="options">
         <div className="top">
           <div className="item">
-            <Link className="link" onClick={() => setKitchen(true)}>KITCHEN</Link>
+            <Link className="link" onClick={() => setKitchen(true)}>
+              <div> KITCHEN </div>
+            </Link>
           </div>
           <div className="item">
-            <Link className="link" onClick={() => setKitchen(false)}>LISTS</Link>
+            <Link className="link" onClick={() => setKitchen(false)}>
+              <div>LISTS</div>
+            </Link>
           </div>
         </div>
       </div>
       {kitchen ? <Options user={user} /> : <ListOptions user={user} />}
-      
     </div>
   );
 };
