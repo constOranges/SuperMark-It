b5848d0c580a04a4ab14178f7f400e5e42d80290
Ryan McMurtry <rkainoam@gmail.com>
2024-01-25 00:19:05 -0800

    configuring image upload with multur, might switch cloudinary instead

--- server/controllers/item.controller.js
+++ server/controllers/item.controller.js
@@ -1,181 +1,244 @@
-const mongoose = require('mongoose');
-const User = require('../models/user.model');
-const Item = require('../models/item.model');
+const mongoose = require("mongoose");
+const User = require("../models/user.model");
+const Item = require("../models/item.model");
+// const multer = require("multer");
+const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "../uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
+//       file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
+
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1024 * 1024 * 5,
+//   },
+// });
 
 // CATEGORIES
 
-module.exports.addItemToCategory = async (req, res) => {
-    const newItem = await Item.create({
-        itemName: req.body.itemName,
-        brand: req.body.brand,
-        quantity: req.body.quantity,
-        expDate: req.body.expDate,
-        notifyDate: req.body.notifyDate,
-        imagePath: req.body.imagePath, // alter to use filepath
-        inUseIDs: [req.body.categoryId] // change this to accommodate multiple categories
-    });
+// app.post('/upload', upload.single('file'), (req, res) => {
+//   Item.create({ imagePath: req.file.filename })
+//     .then((res) => {
+//       res.json(res);
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-    User.updateOne({
-        "_id": req.userId,
-        "categories._id": req.body.categoryId
-    }, {
-        $push: {
-            "categories.$.items": newItem
-        }
-    }, { new: true }).then(user => {
-        res.status(200).json({ message: "Item created succesfully." });
-    }).catch(err => {
-        res.status(400).json(err);
+module.exports.addItemToCategory = async (req, res) => {
+  const newItem = await Item.create({
+    itemName: req.body.itemName,
+    brand: req.body.brand,
+    quantity: req.body.quantity,
+    expDate: req.body.expDate,
+    notifyDate: req.body.notifyDate,
+    imagePath: req.body.imagePath, // alter to use filepath
+    inUseIDs: [req.body.categoryId], // change this to accommodate multiple categories
+  });
+
+  User.updateOne(
+    {
+      _id: req.userId,
+      "categories._id": req.body.categoryId,
+    },
+    {
+      $push: {
+        "categories.$.items": newItem,
+      },
+    },
+    { new: true }
+  )
+    .then((user) => {
+      res.status(200).json({ message: "Item created succesfully." });
     })
-}
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
 
 module.exports.existingItemToCategory = async (req, res) => {
-    const currentItem = await Item.findByIdAndUpdate(req.body.itemId, {
-        $push: {
-            "inUseIDs": req.body.categoryId
-        }
-    }, { new: true });
+  const currentItem = await Item.findByIdAndUpdate(
+    req.body.itemId,
+    {
+      $push: {
+        inUseIDs: req.body.categoryId,
+      },
+    },
+    { new: true }
+  );
 
-    User.updateOne({
-        "_id": req.userId,
-        "categories._id": req.body.categoryId
-    }, {
-        $push: {
-            "categories.$.items": currentItem
-        }
-    }, { new: true }).then(user => {
-        res.status(200).json({ message: "Item added successfully." });
-    }).catch(err => {
-        res.status(400).json(err);
+  User.updateOne(
+    {
+      _id: req.userId,
+      "categories._id": req.body.categoryId,
+    },
+    {
+      $push: {
+        "categories.$.items": currentItem,
+      },
+    },
+    { new: true }
+  )
+    .then((user) => {
+      res.status(200).json({ message: "Item added successfully." });
     })
-}
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
 
 // Consider adding more error handling
 module.exports.removeItemFromCategory = async (req, res) => {
-    try {
-        const currentItem = await Item.findByIdAndUpdate(
-            req.body.itemId,
-            {
-                $pull: {
-                    inUseIDs: req.body.categoryId,
-                },
-            }, {new: true}
-        );
+  try {
+    const currentItem = await Item.findByIdAndUpdate(
+      req.body.itemId,
+      {
+        $pull: {
+          inUseIDs: req.body.categoryId,
+        },
+      },
+      { new: true }
+    );
 
-        await User.updateOne(
-            {
-                "_id": req.userId,
-                "categories._id": req.body.categoryId,
-            },
-            {
-                $pull: {
-                    "categories.$.items": {"_id" : currentItem._id},
-                },
-            }
-        );
+    await User.updateOne(
+      {
+        _id: req.userId,
+        "categories._id": req.body.categoryId,
+      },
+      {
+        $pull: {
+          "categories.$.items": { _id: currentItem._id },
+        },
+      }
+    );
 
-        // deletes item permanently if no longer in use by any categories or lists
-        if (currentItem.inUseIDs.length === 0) {
-            try {
-                await Item.findByIdAndDelete(req.body.itemId);
-                console.log("Item deleted permanently.");
-            } catch (error) {
-                console.error("Error deleting item permanently:", error.message);
-            }
-        }
-
-        res.status(200).json({ message: "Item removed successfully." });
-    } catch (error) {
-        console.error("Error:", error.message);
-        res.status(400).json({ error: error.message });
+    // deletes item permanently if no longer in use by any categories or lists
+    if (currentItem.inUseIDs.length === 0) {
+      try {
+        await Item.findByIdAndDelete(req.body.itemId);
+        console.log("Item deleted permanently.");
+      } catch (error) {
+        console.error("Error deleting item permanently:", error.message);
+      }
     }
+
+    res.status(200).json({ message: "Item removed successfully." });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // LISTS
 
 module.exports.addItemToList = async (req, res) => {
-    const newItem = await Item.create({
-        itemName: req.body.itemName,
-        brand: req.body.brand,
-        quantity: req.body.quantity,
-        expDate: req.body.expDate,
-        notifyDate: req.body.notifyDate,
-        imagePath: req.body.imagePath, // alter to use filepath
-        inUseIDs: [req.body.listId]
-    });
+  const newItem = await Item.create({
+    itemName: req.body.itemName,
+    brand: req.body.brand,
+    quantity: req.body.quantity,
+    expDate: req.body.expDate,
+    notifyDate: req.body.notifyDate,
+    imagePath: req.body.imagePath, // alter to use filepath
+    inUseIDs: [req.body.listId],
+  });
 
-    User.updateOne({
-        "_id": req.userId,
-        "lists._id": req.body.listId
-    }, {
-        $push: {
-            "lists.$.items": newItem
-        }
-    }, { new: true }).then(user => {
-        res.status(200).json({ message: "Item created succesfully." });
-    }).catch(err => {
-        res.status(400).json(err);
+  User.updateOne(
+    {
+      _id: req.userId,
+      "lists._id": req.body.listId,
+    },
+    {
+      $push: {
+        "lists.$.items": newItem,
+      },
+    },
+    { new: true }
+  )
+    .then((user) => {
+      res.status(200).json({ message: "Item created succesfully." });
     })
-}
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
 
 module.exports.existingItemToList = async (req, res) => {
-    const currentItem = await Item.findByIdAndUpdate(req.body.itemId, {
-        $push: {
-            "inUseIDs": req.body.listId
-        }
-    }, { new: true });
+  const currentItem = await Item.findByIdAndUpdate(
+    req.body.itemId,
+    {
+      $push: {
+        inUseIDs: req.body.listId,
+      },
+    },
+    { new: true }
+  );
 
-    User.updateOne({
-        "_id": req.userId,
-        "lists._id": req.body.listId
-    }, {
-        $push: {
-            "lists.$.items": currentItem
-        }
-    }, { new: true }).then(user => {
-        res.status(200).json({ message: "Item added successfully." });
-    }).catch(err => {
-        res.status(400).json(err);
+  User.updateOne(
+    {
+      _id: req.userId,
+      "lists._id": req.body.listId,
+    },
+    {
+      $push: {
+        "lists.$.items": currentItem,
+      },
+    },
+    { new: true }
+  )
+    .then((user) => {
+      res.status(200).json({ message: "Item added successfully." });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
-}
+};
 
 // Consider adding more error handling
 module.exports.removeItemFromList = async (req, res) => {
-    try {
-        const currentItem = await Item.findByIdAndUpdate(
-            req.body.itemId,
-            {
-                $pull: {
-                    inUseIDs: req.body.listId,
-                },
-            }, {new: true}
-        );
+  try {
+    const currentItem = await Item.findByIdAndUpdate(
+      req.body.itemId,
+      {
+        $pull: {
+          inUseIDs: req.body.listId,
+        },
+      },
+      { new: true }
+    );
 
-        await User.updateOne(
-            {
-                "_id": req.userId,
-                "lists._id": req.body.listId,
-            },
-            {
-                $pull: {
-                    "lists.$.items": {"_id" : currentItem._id},
-                },
-            }
-        );
+    await User.updateOne(
+      {
+        _id: req.userId,
+        "lists._id": req.body.listId,
+      },
+      {
+        $pull: {
+          "lists.$.items": { _id: currentItem._id },
+        },
+      }
+    );
 
-        // deletes item permanently if no longer in use by any categories or lists
-        if (currentItem.inUseIDs.length === 0) {
-            try {
-                await Item.findByIdAndDelete(req.body.itemId);
-                console.log("Item deleted permanently.");
-            } catch (error) {
-                console.error("Error deleting item permanently:", error.message);
-            }
-        }
-
-        res.status(200).json({ message: "Item removed successfully." });
-    } catch (error) {
-        console.error("Error:", error.message);
-        res.status(400).json({ error: error.message });
+    // deletes item permanently if no longer in use by any categories or lists
+    if (currentItem.inUseIDs.length === 0) {
+      try {
+        await Item.findByIdAndDelete(req.body.itemId);
+        console.log("Item deleted permanently.");
+      } catch (error) {
+        console.error("Error deleting item permanently:", error.message);
+      }
     }
+
+    res.status(200).json({ message: "Item removed successfully." });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(400).json({ error: error.message });
+  }
 };
