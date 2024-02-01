b9fc20583c73ec9768adeb584dd3203fab6f5680
shu-william <williamshu@outlook.com>
2024-01-31 22:21:32 -0800

    refactored code for unique item IDs

--- server/controllers/item.controller.js
+++ server/controllers/item.controller.js
@@ -25,7 +25,6 @@ module.exports.addItemToCategory = async (req, res) => {
         public_id: result.public_id,
         url: result.secure_url,
       },
-      inUseCategories: [req.body.categoryId],
     });
 
     User.updateOne(
@@ -54,7 +53,6 @@ module.exports.addItemToCategory = async (req, res) => {
       expDate: req.body.expDate,
       notifyDate: req.body.notifyDate,
       // imagePath: req.body.imagePath, change this to be a default icon?
-      inUseCategories: [req.body.categoryId],
     });
 
     User.updateOne(
@@ -79,14 +77,8 @@ module.exports.addItemToCategory = async (req, res) => {
 };
 
 module.exports.existingItemToCategory = async (req, res) => {
-  const currentItem = await Item.findByIdAndUpdate(
-    req.body.itemId,
-    {
-      $push: {
-        inUseCategories: req.body.categoryId,
-      },
-    },
-    { new: true }
+  const currentItem = await Item.findById(
+    req.body.itemId
   );
 
   User.updateOne(
@@ -109,10 +101,6 @@ module.exports.existingItemToCategory = async (req, res) => {
     });
 };
 
-// Must discuss design choice of whether we want to keep items unique across categories and lists or 
-// duplicate items for the sake of easier updating and deleting. For example, we may run into an issue
-// where we want to update an item in one category but since it's being used by multiple categories,
-// do we want it to get updated everywhere? Likely not, although there may be certain beneficial cases.
 module.exports.updateItemInCategory = async (req, res) => {
   const { imagePath } = req.body;
 
@@ -139,27 +127,63 @@ module.exports.updateItemInCategory = async (req, res) => {
       { new: true }
     );
 
-    User.findByIdAndUpdate(
-      req.userId,
+    User.updateOne(
       {
-
-      }
+        _id: req.userId,
+        "categories._id": req.body.categoryId,
+      },
+      {
+        $set: {
+          "categories.$.items": currentItem,
+        }
+      },
+      { upsert: true }
     )
-  }
+      .then((user) => {
+        res.status(200).json({ message: "Item updated successfully." });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      })
+  } else {
+    const currentItem = await Item.findByIdAndUpdate(
+      req.body.itemId,
+      {
+        itemName: req.body.itemName,
+        brand: req.body.brand,
+        quantity: req.body.quantity,
+        expDate: req.body.expDate,
+        notifyDate: req.body.notifyDate,
+      },
+      { new: true }
+    );
 
+    User.updateOne(
+      {
+        _id: req.userId,
+        "categories._id": req.body.categoryId,
+      },
+      {
+        $set: {
+          "categories.$.items": currentItem,
+        }
+      },
+      { upsert: true }
+    )
+      .then((user) => {
+        res.status(200).json({ message: "Item updated successfully." });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      })
+  }
 }
 
 // Consider adding more error handling
 module.exports.removeItemFromCategory = async (req, res) => {
   try {
-    const currentItem = await Item.findByIdAndUpdate(
-      req.body.itemId,
-      {
-        $pull: {
-          inUseCategories: req.body.categoryId,
-        },
-      },
-      { new: true }
+    const currentItem = await Item.findByIdAndDelete(
+      req.body.itemId
     );
 
     await User.updateOne(
@@ -175,17 +199,17 @@ module.exports.removeItemFromCategory = async (req, res) => {
     );
 
     // deletes item permanently if no longer in use by any categories or lists
-    if (
-      currentItem.inUseCategories.length === 0 &&
-      currentItem.inUseLists.length === 0
-    ) {
-      try {
-        await Item.findByIdAndDelete(req.body.itemId);
-        console.log("Item deleted permanently.");
-      } catch (error) {
-        console.error("Error deleting item permanently:", error.message);
-      }
-    }
+    // if (
+    //   currentItem.inUseCategories.length === 0 &&
+    //   currentItem.inUseLists.length === 0
+    // ) {
+    //   try {
+    //     await Item.findByIdAndDelete(req.body.itemId);
+    //     console.log("Item deleted permanently.");
+    //   } catch (error) {
+    //     console.error("Error deleting item permanently:", error.message);
+    //   }
+    // }
 
     res.status(200).json({ message: "Item removed successfully." });
   } catch (error) {
