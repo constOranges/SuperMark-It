a442602192f6f45d195b46e0dd702bb3a8b57fb8
shu-william <williamshu@outlook.com>
2024-03-14 17:25:09 -0700

    fixed missing axios import

--- client/src/components/deleteUser/DeleteUser.jsx
+++ client/src/components/deleteUser/DeleteUser.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./DeleteUser.scss";
 
 const DeleteUser = ({ setPassword, setDeleteToggle, deleteToggle }) => {
